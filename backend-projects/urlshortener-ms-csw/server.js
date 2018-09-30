4d0c76b5068a0aac10ea4a6e5f3af9ebce8fd622
CorySW <corentinwhyte64@gmail.com>
2018-09-30 22:07:24 +0200

    update

--- backend-projects/urlshortener-ms-csw/server.js
+++ backend-projects/urlshortener-ms-csw/server.js
@@ -1,57 +1,60 @@
 const express = require('express');
-const dotenv = require('dotenv');
 const Mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
-dotenv.config();
 const port = process.env.PORT || 3000;
 const Model = require('./UrlDB/Url_short');
-const regex = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+const regex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/gi;
+const UniqID = Math.floor(Math.random() * 100000);
 const db = Mongoose.connection;
 
 db.once("open", () => console.log("yay"))
 db.on("err", (err) => console.log(err))
 Mongoose.connect(process.env.MONGOLAB_URI,{ useNewUrlParser: true });
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-app.get("/lilurl/:MyURL(*)", function(req,res){
-  let { MyURL } = req.params;
-  
-  if(regex.test(MyURL)=== true){
-    let UniqID = Math.floor(Math.random() * 100000).toString();
-
-    let DBdata = new Model(
+app.post("/lilurl/shortenURL", function(req,res){
+  const urlToShorten = req.body.url;
+  let DBdata = new Model(
       {
-        urlInput: MyURL,
+        urlInput: urlToShorten,
         shortened: UniqID
       }
     );
-    Model.findOne({urlInput: MyURL}, (err, url) => {
+  
+  if(regex.test(urlToShorten)=== true){
+    Model.findOne({urlInput: urlToShorten}, (err, url) => {
     if(err) res.send('There was an error while reading through the database');
     if(url == null) {
        DBdata.save()
        res.json({url: DBdata.urlInput, shortened: DBdata.shortened})
        }
     else {
-      res.send("no lol")
+      console.log("already saved")
+      res.json({url: DBdata.urlInput, shortened: DBdata.shortened})
     }
     });
   }
-  res.json({urlInput: 'Invalid Input'});
 });
 
-app.get('/:shorter',(req,res)=> {
-let shorter = req.params;
-
+app.get('/url/:shorter',(req,res)=> {
+let { shorter } = req.params;
+let urlrgx = new RegExp("^(http|https)://","i");
+  
 Model.findOne({shortened: shorter}, (err, data) => {
 if(err) res.send('There was an error while reading through the database');
-let rgx = new RegExp("^(http|https)://","i");
-console.log(data)
-//let checker = data.urlInput;
-//rgx.test(checker) ? res.redirect(302, data.urlInput) : res.redirect(302, 'http://' + data.urlInput);
-});
+var str = data.urlInput;
+  if(urlrgx.test(str)){
+  res.redirect(301, data.urlInput)
+  }
+  else {
+  res.redirect(301, 'http://' + data.urlInput)
+  }
+  }
+);
 });
 
 
